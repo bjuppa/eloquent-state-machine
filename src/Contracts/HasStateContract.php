3292093f0f5f0c1f0a1e9ab0eb379eec6efc340a
Björn Nilsved <nilsved@gmail.com>
2020-10-08 14:42:41 +0200

    Stub out HasState trait & contract

---
+++ src/Contracts/HasStateContract.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Bjuppa\EloquentStateMachine\Contracts;
+
+interface HasStateContract
+{
+    public function getState(); //TODO: set return type
+}
---
+++ src/HasState.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Bjuppa\EloquentStateMachine;
+
+use Exception;
+
+trait HasState
+{
+    public function dispatchToState($event)
+    {
+        try {
+            //TODO: Wrap in transactionWithRefresh
+            $newState = $this->getState()->dispatch($event);
+            $this->refresh();
+            if (get_class($this->getState()) != get_class($newState)) {
+                //TODO: throw UnexpectedStateException
+            }
+            return $newState;
+        } catch (Exception $e) {
+            $this->refresh();
+            throw $e;
+        }
+    }
+
+    protected function makeState($classname)
+    {
+        return new $classname($this);
+    }
+
+    /**
+     * Always save the model to the database using transaction.
+     * @see \Illuminate\Database\Eloquent\Model::save
+     * @see \Illuminate\Database\Eloquent\Model::saveOrFail
+     *
+     * @param  array  $options
+     * @return bool
+     *
+     * @throws \Throwable
+     */
+    public function save(array $options = [])
+    {
+        return $this->getConnection()->transaction(function () use ($options) {
+            return parent::save($options);
+        });
+    }
+
+    /**
+     * Avoid double transactions
+     * @see \Illuminate\Database\Eloquent\Model::saveOrFail
+     *
+     * @param  array  $options
+     * @return bool
+     *
+     * @throws \Throwable
+     */
+    public function saveOrFail(array $options = [])
+    {
+        return $this->save();
+    }
+}
