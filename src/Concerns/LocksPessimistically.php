b10129890913cb3d569da567d43a639f716f2939
Björn Nilsved <nilsved@gmail.com>
2020-10-08 17:04:59 +0200

    Implement complete locking trait

--- src/Concerns/LocksPessimistically.php
+++ src/Concerns/LocksPessimistically.php
@@ -2,19 +2,103 @@
 
 namespace Bjuppa\EloquentStateMachine\Concerns;
 
+use Closure;
+
 trait CanLockPessimistically
 {
     /**
-     * The desired lock type for select statements on this model
+     * Indicates whether row locking is being used.
      *
-     * true: lock for update
+     * true: update lock
      * false: shared lock
      * null: no locking (default)
+     *
+     * @var bool
      */
     protected static $lockPessimistically;
 
     /**
-     * Get a new query builder, having locking selects if desired.
+     * Execute a Closure within a transaction, after refreshing the model and
+     * setting an update lock on the database row.
+     *
+     * @param  \Closure  $callback
+     * @return mixed
+     *
+     * @throws \Throwable
+     */
+    public function transactionWithRefreshForUpdate(Closure $callback)
+    {
+        return $this->getConnection()->transaction(function () use ($callback) {
+            $this->refreshForUpdate();
+            return $callback();
+        });
+    }
+
+    /**
+     * Reload the current model and set update lock on the database row for the
+     * current transaction.
+     * @see \Illuminate\Database\Eloquent\Model::refresh()
+     *
+     * @return $this
+     *
+     * @throws \Throwable
+     */
+    public function refreshForUpdate()
+    {
+        return $this->lockSelectedModelsForUpdate(function () {
+            return $this->refresh();
+        });
+    }
+
+    /**
+     * Execute a Closure, adding update locks to selected models.
+     *
+     * @param  \Closure  $callback
+     * @return mixed
+     *
+     * @throws \Throwable
+     */
+    public function lockSelectedModelsForUpdate(Closure $callback)
+    {
+        return $this->lockSelectedModels(true, $callback);
+    }
+
+    /**
+     * Execute a Closure, adding shared locks to selected models.
+     *
+     * @param  \Closure  $callback
+     * @return mixed
+     *
+     * @throws \Throwable
+     */
+    public function lockSelectedModelsForShare(Closure $callback)
+    {
+        return $this->lockSelectedModels(false, $callback);
+    }
+
+    /**
+     * Execute a Closure, adding locks to selected models.
+     *
+     * @param bool $type
+     * @param  \Closure  $callback
+     * @return mixed
+     *
+     * @throws \Throwable
+     */
+    private function lockSelectedModels(bool $type, Closure $callback)
+    {
+        $previousLock = self::$lockPessimistically;
+        self::$lockPessimistically = $type;
+
+        try {
+            return $callback();
+        } finally {
+            self::$lockPessimistically = $previousLock;
+        }
+    }
+
+    /**
+     * Get a new query builder, having locking selects when applicable.
      * @see \Illuminate\Database\Eloquent\Model::newModelQuery
      *
      * @return \Illuminate\Database\Eloquent\Builder|static
