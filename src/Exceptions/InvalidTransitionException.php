99b23da061272f2f1a2671489c055fb8917e78e9
Björn Nilsved <nilsved@gmail.com>
2020-10-15 14:06:51 +0200

    Store transition in InvalidTransitionException

--- src/Exceptions/InvalidTransitionException.php
+++ src/Exceptions/InvalidTransitionException.php
@@ -2,12 +2,17 @@
 
 namespace Bjuppa\EloquentStateMachine\Exceptions;
 
+use Bjuppa\EloquentStateMachine\Support\Transition;
 use DomainException;
 
 class InvalidTransitionException extends DomainException
 {
-    public function __construct()
+    public Transition $transition;
+
+    public function __construct(Transition $transition)
     {
+        $this->transition = $transition;
+
         parent::__construct("Could not transition to desired state.");
     }
 }
