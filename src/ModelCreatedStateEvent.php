19025b20d747aff5095acba1c70c92407fb617e5
Björn Nilsved <nilsved@gmail.com>
2020-10-16 14:38:00 +0200

    Document overriding

--- src/ModelCreatedStateEvent.php
+++ src/ModelCreatedStateEvent.php
@@ -4,4 +4,10 @@ namespace Bjuppa\EloquentStateMachine;
 
 class ModelCreatedStateEvent extends StateEvent
 {
+    /**
+     * Extend this class and return an instance from initialTransitionEvent()
+     * in your model if you need any special payload to be sent along with
+     * newly created models when the state machine does the first
+     * transition to the initial state.
+     */
 }
